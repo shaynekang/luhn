672b46976c38dd76d9e23d07fc500508801ba22b
paranoiase Kang <paranoiase@gmail.com>
2012-06-28 01:43:51 +0900

    number should convert credit card

--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -1,10 +1,26 @@
 class CreditCard
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def ==(another)
+    number == another.number
+  end
+
   def valid?
     true
   end
 end
 
 describe CreditCard do
+  describe "#==" do
+    it "should be equal credit card if number is identical" do
+      CreditCard.new(37).should == CreditCard.new(37)
+    end
+  end
+
   describe "#valid?" do
     it "should validate credit card number" do
       CreditCard.new(0).should be_valid
@@ -13,12 +29,22 @@ describe CreditCard do
 end
 
 class Fixnum
+  def to_credit_card
+    CreditCard.new(self)
+  end
+
   def luhn?
-    CreditCard.new(self).valid?
+    to_credit_card.valid?
   end
 end
 
 describe Fixnum do
+  describe "#to_credit_card" do
+    it "should convert number to credit card" do
+      37.to_credit_card.should == CreditCard.new(37)
+    end
+  end
+
   describe "#luhn?" do
     it "should validate credit card number" do
       0.should be_luhn
