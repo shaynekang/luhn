cc31dd6db4f7efe5a2df4c3bf06d9b8388ba3a5c
paranoiase Kang <paranoiase@gmail.com>
2012-06-28 21:19:57 +0900

    rename credit card's number to card_number

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -2,16 +2,16 @@ require 'ruby_extensions/array'
 require 'ostruct'
 
 class CreditCard
-  attr_reader :number
+  attr_reader :card_number
 
   DEFAULT_OPTIONS = {check_digit: 0}
-  def initialize(number, opts={})
-    @number = number
+  def initialize(card_number, opts={})
+    @card_number = card_number
     @options = OpenStruct.new(DEFAULT_OPTIONS.merge(opts))
   end
 
   def ==(another)
-    number == another.number
+    card_number == another.card_number
   end
 
   def valid?
@@ -37,7 +37,7 @@ class CreditCard
     to_digits(double_every_second_digit).inject(:+)
   end
 
-  def to_digits(number=@number)
+  def to_digits(number = card_number)
     number.to_s.chars.to_a.map(&:to_i)
   end
 end
